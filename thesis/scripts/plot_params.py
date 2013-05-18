057ae1f814c0181cfa6c6abf0a4e8e854347d092
Nicolas Bigaouette <nbigaouette@gmail.com>
2013-05-17 23:16:48 -0400

    New script: set plot parameters

---
+++ thesis/scripts/plot_params.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import matplotlib
+
+matplotlib.rcParams['figure.figsize']           = (12.0, 7.0)
+matplotlib.rcParams['text.usetex']              = True
+matplotlib.rcParams['font.size']                = 24
+matplotlib.rcParams['font.family']              = 'serif'
+matplotlib.rcParams['font.serif']               = 'computer modern roman'
+matplotlib.rcParams['lines.linewidth']          = 6
+matplotlib.rcParams['figure.subplot.bottom']    = 0.15
