332500d7245d06341e1a123348959bf0e4aac16b
Nicolas Bigaouette <nbigaouette@gmail.com>
2013-06-17 22:33:31 -0400

    Move arrow class to own file

--- thesis/scripts/aci.py
+++ thesis/scripts/aci.py
@@ -10,6 +10,7 @@ import numpy as np
 import plot
 import plot_params
 
+import fleches
 import colors_and_symbols
 import constants as cst
 
@@ -17,52 +18,6 @@ import constants as cst
 plot.matplotlib.rcParams['figure.subplot.hspace'] = 0.0
 plot.matplotlib.rcParams['figure.subplot.wspace'] = 0.0
 
-class arrow:
-    def __init__(self, name, start_xy, stop_xy):
-        self.name       = name
-        self.start_xy   = np.asarray(start_xy)
-        self.stop_xy    = np.asarray(stop_xy)
-        self.length     = np.sqrt(((self.stop_xy - self.start_xy)**2).sum())
-    def PrintHeader(self):
-        print "Arrow name           Start         Stop         Length            Start          Stop         Length"
-    def Print(self):
-        print str('%-16s' % self.name) + " " + \
-                    str('%8.4f' % self.start_xy) + " Eh   " + \
-                    str('%8.4f' % self.stop_xy) + " Eh   " + \
-                    str('%8.4f' % self.length) + " Eh   " + \
-                "    " + \
-                    str('%8.4f' % (self.start_xy * cst.Eh_to_eV)) + " eV   " + \
-                    str('%8.4f' % (self.stop_xy * cst.Eh_to_eV)) + " eV   " + \
-                    str('%8.4f' % (self.length * cst.Eh_to_eV)) + " eV"
-    def Is_Null(self):
-        if (self.length < 1.0e-5):
-            return True
-        else:
-            return False
-    def Plot(self, ax, color = 'k', label = None,
-             horizontalalignment = 'center', verticalalignment = 'center',
-             bidirectional = False):
-        if (self.Is_Null()):
-            label_equal_zero = r" = 0"
-        else:
-            label_equal_zero = r""
-            if (bidirectional):
-                arrowstyle = "<|-|>"
-            else:
-                arrowstyle = "-|>"
-            ann = ax.annotate('', xy     = self.stop_xy,  xycoords='data',
-                                  xytext = self.start_xy, textcoords='data',
-                                  arrowprops=dict(arrowstyle = arrowstyle, color = color),
-                                  annotation_clip=False )
-            ann.arrow_patch.set_clip_box(ax.bbox)
-            #ax.plot([self.start_xy[0]], [self.start_xy[1]], 'o' + color)
-        if (label != None):
-            dxy = self.stop_xy + self.start_xy
-            ax.text(dxy[0]/2.0, dxy[1]/2.0,
-                    label + label_equal_zero,
-                    horizontalalignment = horizontalalignment,
-                    verticalalignment = verticalalignment,
-                    color = color)
 
 
 Xe_Z0_Ip = 12.265625 * cst.eV_to_Eh
@@ -142,8 +97,8 @@ for ei in xrange(len(Xe_Z0_es_E)):
                                 label = Xe_Z0_es_n[ei])
 
 # Impacting electron
-ar1Ke = arrow('K_e', [0.75*r[0], 0.0], [0.75*r[0], Ke])    # Vertical (Ke)
-ar2Ke = arrow('K_e', [0.75*r[0], Ke],  [0.75*r[0]+10, Ke]) # Horizontal (v)
+ar1Ke = fleches.arrow('K_e', [0.75*r[0], 0.0], [0.75*r[0], Ke])    # Vertical (Ke)
+ar2Ke = fleches.arrow('K_e', [0.75*r[0], Ke],  [0.75*r[0]+10, Ke]) # Horizontal (v)
 ar1Ke.Plot(ax1, color = 'b', label = '$K_e$', horizontalalignment = 'right', bidirectional = True)
 ar2Ke.Plot(ax1, color = 'b', label = '$v_e$', verticalalignment = 'top')
 ax1.plot([0.75*r[0]], [Ke], 'ob', ms = 14)
@@ -161,8 +116,8 @@ for ei in xrange(len(Xe_Z0_es_E)):
                                 label = Xe_Z0_es_n[ei])
 # Impacting electron
 Kep = Ke - Xe_Z0_gses[0]
-ar1Ke = arrow("K_e'", [0.0, 0.0], [0.0, Kep]) # Vertical (Ke)
-ar2Ke = arrow("K_e'", [0.0, Kep], [5.0, Kep]) # Horizontal (v)
+ar1Ke = fleches.arrow("K_e'", [0.0, 0.0], [0.0, Kep]) # Vertical (Ke)
+ar2Ke = fleches.arrow("K_e'", [0.0, Kep], [5.0, Kep]) # Horizontal (v)
 ar1Ke.Plot(ax2, color = 'b', label = r"$K_e'$   .", horizontalalignment = 'right', bidirectional = True)
 ar2Ke.Plot(ax2, color = 'b', label = r"$v_e'$", verticalalignment = 'bottom')
 ax2.plot([0.0], [Kep], 'ob', ms = 14)
@@ -170,7 +125,7 @@ ax2.plot([0.0], [Kep], 'ob', ms = 14)
 # Bound electron
 ax2.plot([0.0], [Xe_Z0_es_E[0]], 'og', ms = 14)
 ax2.plot([0.0], [-Xe_Z0_Ip], 'og', ms = 14, alpha = 0.4)
-arBe = arrow("", [0.0, -Xe_Z0_Ip], [0.0, Xe_Z0_es_E[0]]) # Vertical
+arBe = fleches.arrow("", [0.0, -Xe_Z0_Ip], [0.0, Xe_Z0_es_E[0]]) # Vertical
 arBe.Plot(ax2, color = 'g')
 
 
@@ -183,15 +138,15 @@ for ei in xrange(len(Xe_Z0_es_E)):
                                 label = Xe_Z0_es_n[ei])
 # Impacting electron
 Kep = Ke - Xe_Z0_gses[0]
-ar1Ke = arrow("K_e'", [0.75*r[-1], 0.0], [0.75*r[-1], Kep]) # Vertical (Ke)
-ar2Ke = arrow("K_e'", [0.75*r[-1], Kep], [0.75*r[-1]+5.0, Kep]) # Horizontal (v)
+ar1Ke = fleches.arrow("K_e'", [0.75*r[-1], 0.0], [0.75*r[-1], Kep]) # Vertical (Ke)
+ar2Ke = fleches.arrow("K_e'", [0.75*r[-1], Kep], [0.75*r[-1]+5.0, Kep]) # Horizontal (v)
 ar1Ke.Plot(ax3, color = 'b', label = r"$K_e'$   .", horizontalalignment = 'right', bidirectional = True)
 ar2Ke.Plot(ax3, color = 'b', label = r"$v_e'$", verticalalignment = 'bottom')
 ax3.plot([0.75*r[-1]], [Kep], 'ob', ms = 14)
 
 # New Impacting electron
-ar1Ke = arrow('K_{e,2}', [0.75*r[0], 0.0], [0.75*r[0], Ke2])    # Vertical (Ke)
-ar2Ke = arrow('K_{e,2}', [0.75*r[0], Ke2],  [0.75*r[0]+5, Ke2]) # Horizontal (v)
+ar1Ke = fleches.arrow('K_{e,2}', [0.75*r[0], 0.0], [0.75*r[0], Ke2])    # Vertical (Ke)
+ar2Ke = fleches.arrow('K_{e,2}', [0.75*r[0], Ke2],  [0.75*r[0]+5, Ke2]) # Horizontal (v)
 ar1Ke.Plot(ax3, color = 'r', label = '$K_{e,2}$', horizontalalignment = 'right', bidirectional = True)
 ar2Ke.Plot(ax3, color = 'r', label = '$v_{e,2}$', verticalalignment = 'top')
 ax3.plot([0.75*r[0]], [Ke2], 'or', ms = 14)
@@ -209,8 +164,8 @@ for ei in xrange(len(Xe_Z0_es_E)):
                                 label = Xe_Z0_es_n[ei])
 
 # New Impacting electron
-#ar1Ke = arrow('K_{e,2}', [0.0, 0.0], [0.0, Ke2])    # Vertical (Ke)
-ar2Ke = arrow('K_{e,2}', [0.0, Ke2],  [0.0+2.5, Ke2]) # Horizontal (v)
+#ar1Ke = fleches.arrow('K_{e,2}', [0.0, 0.0], [0.0, Ke2])    # Vertical (Ke)
+ar2Ke = fleches.arrow('K_{e,2}', [0.0, Ke2],  [0.0+2.5, Ke2]) # Horizontal (v)
 #ar1Ke.Plot(ax4, color = 'r', label = "$K'_{e,2}$", horizontalalignment = 'right', bidirectional = True)
 ar2Ke.Plot(ax4, color = 'r', label = "$v'_{e,2}$", verticalalignment = 'top')
 ax4.plot([0.0], [Ke2], 'or', ms = 14)
@@ -218,9 +173,9 @@ ax4.plot([0.0], [Ke2], 'or', ms = 14)
 ## Bound electron, now free
 ax4.plot([0.0], [Xe_Z0_es_E[0]], 'og', ms = 14, alpha = 0.4)
 ax4.plot([0.0], [Ke2+Xe_Z0_es_E[0]], 'og', ms = 14)
-ar1Ke = arrow('', [0.0, Xe_Z0_es_E[0]], [0.0, Ke2+Xe_Z0_es_E[0]])    # Vertical (Ke)
+ar1Ke = fleches.arrow('', [0.0, Xe_Z0_es_E[0]], [0.0, Ke2+Xe_Z0_es_E[0]])    # Vertical (Ke)
 ar1Ke.Plot(ax4, color = 'g')
-ar2Ke = arrow('', [0.0, Ke2+Xe_Z0_es_E[0]],  [0.0+2.5, Ke2+Xe_Z0_es_E[0]]) # Horizontal (v)
+ar2Ke = fleches.arrow('', [0.0, Ke2+Xe_Z0_es_E[0]],  [0.0+2.5, Ke2+Xe_Z0_es_E[0]]) # Horizontal (v)
 ar2Ke.Plot(ax4, color = 'g', label = "$v_{e,3}$", verticalalignment = 'top')
 
 for ax in [ax1, ax2, ax3, ax4]:
---
+++ thesis/scripts/fleches.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+import constants as cst
+
+
+class arrow:
+    def __init__(self, name, start_xy, stop_xy):
+        self.name       = name
+        self.start_xy   = np.asarray(start_xy)
+        self.stop_xy    = np.asarray(stop_xy)
+        self.length     = np.sqrt(((self.stop_xy - self.start_xy)**2).sum())
+    def PrintHeader(self):
+        print "Arrow name           Start         Stop         Length            Start          Stop         Length"
+    def Print(self):
+        print str('%-16s' % self.name) + " " + \
+                    str('%8.4f' % self.start_xy) + " Eh   " + \
+                    str('%8.4f' % self.stop_xy) + " Eh   " + \
+                    str('%8.4f' % self.length) + " Eh   " + \
+                "    " + \
+                    str('%8.4f' % (self.start_xy * cst.Eh_to_eV)) + " eV   " + \
+                    str('%8.4f' % (self.stop_xy * cst.Eh_to_eV)) + " eV   " + \
+                    str('%8.4f' % (self.length * cst.Eh_to_eV)) + " eV"
+    def Is_Null(self):
+        if (self.length < 1.0e-5):
+            return True
+        else:
+            return False
+    def Plot(self, ax, color = 'k', label = None,
+             horizontalalignment = 'center', verticalalignment = 'center',
+             bidirectional = False):
+        if (self.Is_Null()):
+            label_equal_zero = r" = 0"
+        else:
+            label_equal_zero = r""
+            if (bidirectional):
+                arrowstyle = "<|-|>"
+            else:
+                arrowstyle = "-|>"
+            ann = ax.annotate('', xy     = self.stop_xy,  xycoords='data',
+                                  xytext = self.start_xy, textcoords='data',
+                                  arrowprops=dict(arrowstyle = arrowstyle, color = color),
+                                  annotation_clip=False )
+            ann.arrow_patch.set_clip_box(ax.bbox)
+            #ax.plot([self.start_xy[0]], [self.start_xy[1]], 'o' + color)
+        if (label != None):
+            dxy = self.stop_xy + self.start_xy
+            ax.text(dxy[0]/2.0, dxy[1]/2.0,
+                    label + label_equal_zero,
+                    horizontalalignment = horizontalalignment,
+                    verticalalignment = verticalalignment,
+                    color = color)
